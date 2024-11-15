34a5e176d8fb58215fc84260f86174255157048c
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2024-11-14 21:33:58 -0500

    added files

---
+++ rateflix/src/UserInterface/RegisterUI.java
@@ -0,0 +1,11 @@
+package UserInterface;
+
+import base.*;
+
+public class RegisterUI {
+    private User user;
+
+    public RegisterUI(User user){
+        this.user = user;
+    }
+}
---
+++ rateflix/src/base/Movie.java
@@ -0,0 +1,58 @@
+package base;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Movie {
+	private String title;
+	private String director;
+	private String[] cast;
+	private Date date;
+	
+	public Movie(String title, String director, String[] cast, Date date) {
+		this.title = title;
+		this.director = director;
+		this.cast = cast;
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String[] getCast() {
+		return cast;
+	}
+
+	public void setCast(String[] cast) {
+		this.cast = cast;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [title=" + title + ", director=" + director + ", cast=" + Arrays.toString(cast) + ", date=" + date
+				+ "]";
+	}
+	
+	
+}
