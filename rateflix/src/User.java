6df08ff88bba90b5009e965f582d53c05319005c
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2024-11-14 19:11:23 -0500

    user class made

---
+++ rateflix/src/User.java
@@ -0,0 +1,32 @@
+public class User {
+	private String userName;
+	private String Password;
+	
+	public User (String userName, String Password) {
+		this.userName = userName;
+		this.Password = Password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", Password=" + Password + "]";
+	}
+	
+	
+}
